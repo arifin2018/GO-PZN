899672570f5979d06138cd0ed3aaf7333a19a49d
arifin2018 <arifingdr@gmail.com>
2023-11-13 02:15:25 +0700

    update string

---
+++ string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("nur")
+	fmt.Println("arifin")
+
+	fmt.Println(len("arifin"))
+	fmt.Println(string("arifin"[0]))
+}
--- vowelConsonant.go
+++ vowelConsonant.go
@@ -3,52 +3,59 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 )
 
-type sortArray struct{
-	Name string
-	Number int
-}
 
 func main() {
-	// var input1 string = "Next Case"
-	var result map[string]int;
-	result= map[string]int{"orange": 5, "apple": 7,"mango": 3, "strawberry": 9}
-	// apple => apple, orange, mango, strawberry
-	// orange =>  apple, orange, mango, strawberry
-	// strawberry => apple, orange, strawberry, mango
-	// strawberry => apple, strawberry, orange, mango
-	// strawberry => strawberry: 9, apple: 7 , orange: 5, mango: 3
-
-
+	var input1 string = strings.ToLower("Sample Case")
 	
-	keys := make([]string, 0, len(result))
-	for key,_ := range result {
-        keys = append(keys, key)
-    }
 
-	sort.Slice(keys, func(i, j int) bool {
-		fmt.Println("i ",i)
-		fmt.Println("j ",j)
-		fmt.Println("result[keys[i]] ",result[keys[i]])
-		fmt.Println("result[keys[j] ",result[keys[j]])
-		fmt.Println("result[keys[i]] > result[keys[j] ",result[keys[i]] > result[keys[j]])
-		fmt.Println("keys ",keys)
-		fmt.Println("result ",result)
-		fmt.Println()
-		return result[keys[i]] > result[keys[j]]
+	var vocal map[string]int
+	vocal = map[string]int{}
+	var consonant map[string]int
+	consonant = map[string]int{}
+
+	for i := 0; i < len(input1); i++ {
+		if string(input1[i]) == "a" || string(input1[i]) == "i" || string(input1[i]) == "u" || string(input1[i]) == "e" || string(input1[i]) == "o"{
+			vocal[string(input1[i])] = vocal[string(input1[i])] + 1 
+		}else if string(input1[i]) != " "{
+			consonant[string(input1[i])] = consonant[string(input1[i])] + 1 
+		}
+	}
+	
+
+	keysVocal := make([]string, 0, len(vocal))
+	for k := range vocal {
+		keysVocal = append(keysVocal, k)
+	}
+
+	sort.SliceStable(keysVocal, func(i, j int) bool {
+		return vocal[keysVocal[i]] > vocal[keysVocal[j]]
 	})
-	// fmt.Println("==================")
-	// fmt.Println(result)
-	// fmt.Println(keys)
-	for _, k := range keys{
-        fmt.Println(k, result[k])
-    }
 
-	// for i := 0; i < strings.Count(input1,"")-1; i++ {
-	// 	if string(input1[i]) == "a" || string(input1[i]) == "i" || string(input1[i]) == "u" || string(input1[i]) == "e" || string(input1[i]) == "o" {
-	// 		// result += string(input1[i])
-	// 		result[string(input1[i])]++
-	// 	}
-	// }
+	keysConsonant := make([]string, 0, len(consonant))
+	for k := range consonant {
+		keysConsonant = append(keysConsonant, k)
+	}
+
+	sort.SliceStable(keysConsonant, func(i, j int) bool {
+		return consonant[keysConsonant[i]] > consonant[keysConsonant[j]]
+	})
+
+	fmt.Print("vocal => ")
+	for _, v := range keysVocal {
+		for i := 0; i < vocal[v]; i++ {
+			fmt.Printf("%s",v)
+		}
+		continue
+	}
+	fmt.Println()
+	fmt.Print("consonant => ")
+	for _, v := range keysConsonant {
+		for i := 0; i < consonant[v]; i++ {
+			fmt.Printf("%s",v)
+		}
+		continue
+	}
 }
