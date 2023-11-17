3c3a4a72e1ec234e547686adc1cbe5e0a93753d0
arifinlenna <arifin@lenna.ai>
2023-11-17 10:10:25 +0700

    update breakContinue

---
+++ switch.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	var name = "arifin"
+
+	switch name {
+	case "arifin":
+		fmt.Println("arifin")
+	default:
+		fmt.Println("else")
+	}
+
+	switch length:= len(name); length > 5 {
+	case true:
+		fmt.Println("lebih dari 5")
+	case false:
+		fmt.Println("kurang dari 5")
+	}
+
+	switch {
+	case string(name[0]) == "a":
+		fmt.Println("a")
+	case string(name[0]) == "b":
+		fmt.Println("b")
+	default:
+		fmt.Println("c")
+	}
+}
