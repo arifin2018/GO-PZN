7dfdb5729983c38c6d19bc949d065d01567bf737
arifinlenna <arifin@lenna.ai>
2023-12-07 08:47:06 +0700

    typeAssertions

---
+++ interfaceKosong.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func testInterfaceKosong() any { //any disini sama saja seperti interface{}
+	// return true
+	// return "a"
+	return 1
+}
+
+func main()  {
+	fmt.Println(testInterfaceKosong())
+	
+}
