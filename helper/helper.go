0c7e8a7eb9a54858315de0195607d745b9fdbb63
arifinlenna <arifin@lenna.ai>
2023-12-11 17:49:13 +0700

    packageAndImport

---
+++ helper/helper.go
@@ -0,0 +1,5 @@
+package helper
+
+func SayHello(name string) string { //depan nya harus huruf besar supaya bisa di akses di file lainnya
+	return "Hello " + name
+}
---
+++ packageAndImport.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"GO-PZN/helper"
+	"fmt"
+)
+
+func main()  {
+	result := helper.SayHello("arifin")
+	fmt.Println(result)
+}
