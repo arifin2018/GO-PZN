32d09e8b6a6d3ccf8049bdb300e30324701e5004
arifin2018 <arifingdr@gmail.com>
2024-01-13 12:42:19 +0700

    update file

---
+++ file.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func createNewFile(name string, message string) error {
+	file, err := os.OpenFile(name,os.O_CREATE|os.O_RDWR,0755)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	file.Write([]byte(message))
+	return nil
+}
+
+func appendNewLineFile(name string,message string) error {
+	file, err := os.OpenFile(name,os.O_CREATE|os.O_APPEND,0755)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	file.WriteString(message)
+	return nil
+}
+
+func readFile(name string) (string,error) {
+	file,err := os.OpenFile(name, os.O_RDONLY, 0444)
+	if err != nil {
+		return "",err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	var message string
+	for {
+		line,_,err := reader.ReadLine()
+		if err != nil {
+			return "", err
+		}
+		message += string(line)
+	}
+	return message, nil
+}
+
+func main() {
+	// createNewFile("golang.log","nur arifin")
+	// appendNewLineFile("golang.log","\nnur arifin 2")
+	readFile("golang.log")
+}
