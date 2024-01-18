db89a734861425011a0299212b4425170ac4fbdf
arifin2018 <arifingdr@gmail.com>
2024-01-18 19:36:31 +0700

    table TestTableTest

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,12 +9,48 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestTableTest(t *testing.T)  {
+	tests := []struct{
+		name string
+		request string
+		expected string
+	}{
+		{
+			name:"nur",
+			request:"nur",
+			expected:"Hello nur",
+		},
+		{
+			name:"arifin",
+			request:"arifin",
+			expected:"Hello arifin",
+		},
+	}
+	for _, v := range tests {
+		t.Run(v.name, func(t *testing.T) {
+			result := HelloWorld(v.request)
+			require.Equal(t, v.expected, result);
+		})
+	}
+}
+
 func TestMain(t *testing.M)  {
 	fmt.Println("FIRST arifin")
 	
 	t.Run()
 	
 	fmt.Println("END arifin")
+}
+
+func TestSubTest(t *testing.T)  {
+	t.Run("Nur", func(t *testing.T) {
+		result := HelloWorld("arifin")
+		require.Equal(t, "Hello arifin", result);
+	})
+	t.Run("Arifin", func(t *testing.T) {
+		result := HelloWorld("arifin")
+		require.Equal(t, "Hello arifin", result);
+	})
 }
 
 func TestSkip(t *testing.T){
