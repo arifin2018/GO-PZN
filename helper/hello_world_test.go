a9e90382d2385d5620539b5be7f339e3d4a2509d
arifin2018 <arifingdr@gmail.com>
2024-01-18 16:33:03 +0700

    Before After unit test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,9 +1,35 @@
 package helper
 
 import (
+	"fmt"
+	"runtime"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+func TestMain(t *testing.M)  {
+	fmt.Println("FIRST arifin")
+	
+	t.Run()
+	
+	fmt.Println("END arifin")
+}
+
+func TestSkip(t *testing.T){
+	if runtime.GOOS == "windows" {
+		t.Skip("can't running in windows,please use another os")
+	}
+	result := HelloWorld("arifin")
+	require.Equal(t, "Hello arifin", result);
+}
+
+func TestHelloWorldRequire(t *testing.T)  {
+	result := HelloWorld("arifin")
+
+	require.Equal(t, "Hello arifin", result);
+}
 func TestHelloWorld(t *testing.T)  {
 	result := HelloWorld("arifin")
 
@@ -12,4 +38,10 @@ func TestHelloWorld(t *testing.T)  {
 		// panic("error")
 		t.Error("hai")
 	}
+}
+
+func TestHelloWorldAssertion(t *testing.T)  {
+	result := HelloWorld("arifin")
+
+	assert.Equal(t, "Hello arifin", result)
 }
