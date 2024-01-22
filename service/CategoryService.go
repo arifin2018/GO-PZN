72cc9d928684c468498293cb9243ca1b6bb49f9d
arifinlenna <arifin@lenna.ai>
2024-01-22 09:39:19 +0700

    update

--- service/CategoryService.go
+++ service/CategoryService.go
@@ -13,7 +13,7 @@ type CategoryService struct {
 func (service CategoryService) Get(id string) (*entity.Category, error) {
 	category := service.Repository.FindById(id)
 	if category == nil {
-		return nil, errors.New("Category Not Found")
+		return nil, errors.New("category not found")
 	}else{
 		return category, nil
 	}
