571830c9c81c697befae91ea06246aa9b8de46ec
arifinlenna <arifin@lenna.ai>
2024-01-24 21:30:31 +0700

    default select channel

--- Channel_test.go
+++ Channel_test.go
@@ -109,3 +109,36 @@ func TestRangeChannel(t *testing.T)  {
 	fmt.Println("===============================SELESAI==========================")
 
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Nur Arifin"
+}
+
+func TestSelectChannel(t *testing.T)  {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	var channelNumber int
+	for {
+		select{
+		case data := <- channel1:
+			fmt.Println("Data dari channel 1", data)
+			channelNumber++
+		case data := <- channel2:
+			fmt.Println("Data dari channel 2", data)
+			channelNumber++
+		default:
+			fmt.Println("Menunggu Data")
+		}
+		if channelNumber == 2{
+			break;
+		}
+	}
+
+}
