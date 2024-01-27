66df8a726a63872155dea33afaf6d7cd143cf54a
arifin2018 <arifingdr@gmail.com>
2024-01-28 04:24:47 +0700

    ContextTimeout

--- ContextTimeout_test.go
+++ ContextTimeout_test.go
@@ -11,19 +11,33 @@ import (
 func TestContextTimeout(t *testing.T) {
 	fmt.Println("Total Golang Goroutine", runtime.NumGoroutine())
 	parent := context.Background()
-	ctx, cancel := context.WithTimeout(parent, 1*time.Second)
+	ctx, cancel := context.WithTimeout(parent, 3*time.Nanosecond)
 	defer cancel()
 	destinationChan := make(chan int)
+	
 	destination := createCounter(destinationChan,ctx)
 	
 	fmt.Println(<- destination)
 	for v := range destination {
+		select {
+		case <-time.After(1*time.Nanosecond):
+			break
+		case <-ctx.Done():
+			break
+		}
 		fmt.Println("counter ", v)
-		if v == 2 {
+		if v == 29 {
 			break;
 		}
 	}
+	
+	
 	time.Sleep(1*time.Nanosecond)
 
 	fmt.Println("Total Golang Goroutine", runtime.NumGoroutine())
+}
+
+func f(ctx context.Context) {    
+	deadline,_:=ctx.Deadline()
+	fmt.Println(time.Until(deadline))
 }
