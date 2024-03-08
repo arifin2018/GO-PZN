bb767f2c31c863d00f68a0c3ed5d9f7c5bc3d7c3
arifinlenna <arifin@lenna.ai>
2024-03-08 22:50:03 +0700

    TestServerFile

--- serve_file_test.go
+++ serve_file_test.go
@@ -24,18 +24,3 @@ func TestServerFile(t *testing.T) {
 
 	assert.Equal(t, "hallo arifin", string(responseBody))
 }
-
-func TestServerFile2(t *testing.T) {
-	router := httprouter.New()
-	router.ServeFiles("/src/*filepath", http.Dir("./resources"))
-
-	request := httptest.NewRequest("GET", "http://localhost/src/hello.txt", nil)
-	recorder := httptest.NewRecorder()
-
-	router.ServeHTTP(recorder, request)
-
-	response := recorder.Result()
-	responseBody, _ := io.ReadAll(response.Body)
-
-	assert.Equal(t, "hallo arifin", string(responseBody))
-}
