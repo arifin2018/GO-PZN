968e64ffcc977e84532a1fce4f4692cbd956db30
arifinlenna <arifin@lenna.ai>
2024-03-08 22:50:00 +0700

    TestServerFile

---
+++ serve_file_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServerFile(t *testing.T) {
+	router := httprouter.New()
+	router.ServeFiles("/src/*filepath", http.Dir("./resources"))
+
+	request := httptest.NewRequest("GET", "http://localhost/src/hello.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	responseBody, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "hallo arifin", string(responseBody))
+}
+
+func TestServerFile2(t *testing.T) {
+	router := httprouter.New()
+	router.ServeFiles("/src/*filepath", http.Dir("./resources"))
+
+	request := httptest.NewRequest("GET", "http://localhost/src/hello.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	responseBody, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "hallo arifin", string(responseBody))
+}
