ac1303ef86b035abeef4b78b34ffce67cf2a8dfb
arifinlenna <arifin@lenna.ai>
2024-03-11 03:57:52 +0700

    TestUnmarshalObjectMap

--- json_unmarshal_test.go
+++ json_unmarshal_test.go
@@ -6,12 +6,25 @@ import (
 	"testing"
 )
 
-func TestUnmarshalObject(t *testing.T) {
+func TestUnmarshalObjectStruct(t *testing.T) {
 	data := `{"Firstname":"Nur","Lastname":"Arifin","Age":30,"Married":false}`
 	bytes := []byte(data)
 
 	customer := &Customer{}
 	err := json.Unmarshal(bytes, customer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(customer)
+	bytes2, _ := json.Marshal(customer)
+	fmt.Println(string(bytes2))
+}
+func TestUnmarshalObjectMap(t *testing.T) {
+	data := `{"Firstname":"Nur","Lastname":"Arifin","Age":30,"Married":false}`
+	bytes := []byte(data)
+
+	customer := map[string]any{}
+	err := json.Unmarshal(bytes, &customer)
 	if err != nil {
 		panic(err)
 	}
